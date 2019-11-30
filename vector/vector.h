9d87857af5c8482bbb39d4200c3cf8a9f0448065
Luiserebii <luis@serebii.io>
2019-11-30 02:41:49 -0500

    Setting up vector implementation

---
+++ vector/vector.h
@@ -0,0 +1,32 @@
+#ifndef LUISEREBII_VECTOR_H
+#define LUISEREBII_VECTOR_H
+
+#include <memory>
+
+namespace lsb {
+
+template <class T>
+class vector {
+
+    public:
+        vector(): head(0), tail(0) { } 
+
+        void push_back(T e);
+
+    private:
+        T* head;
+        T* tail;
+
+        void construct();
+        void destroy();
+
+}
+
+}
+
+template <class T>
+vector::push_back() {
+    
+}
+
+#endif
