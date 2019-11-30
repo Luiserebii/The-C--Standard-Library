e0ffea5ec8ddb5a4d8fb17afc45b1c748ba47792
Luiserebii <luis@serebii.io>
2019-11-30 04:58:54 -0500

    Adding find(), and correcting some implementation details

--- algorithm/algorithm.h
+++ algorithm/algorithm.h
@@ -5,15 +5,21 @@ namespace lsb {
 
 template <class In>
 bool equal(In begin, In end, In begin2) {
-    while(begin != end) {
-        if(begin++ != begin2++) {
+    while(*begin != *end) {
+        if(*begin != *begin2) {
             return false;
         }
+        ++begin, ++begin2;
     }
     return true;
 }
 
-
+template <class In, class T>
+In find(In begin, In end, T val) {
+    while(begin != end && *begin != val) {
+        ++begin;
+    }
+    return begin;
 }
 
 #endif
