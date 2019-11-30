bb71c32ee64dfec1db783ba08ee9df01bd2f81c4
Luiserebii <luis@serebii.io>
2019-11-30 03:07:17 -0500

    Continuing to flesh out vector, especially push_back()

--- vector/vector.h
+++ vector/vector.h
@@ -1,6 +1,7 @@
 #ifndef LUISEREBII_VECTOR_H
 #define LUISEREBII_VECTOR_H
 
+#include <algorithm>
 #include <memory>
 
 namespace lsb {
@@ -9,25 +10,58 @@ template <class T>
 class vector {
 
     public:
-        vector(): head(0), tail(0) { } 
+        typedef size_t size_type;
 
+        vector(): head(0), avail(0), tail(0) { } 
+        
+        size_t size();
         void push_back(T e);
 
     private:
         T* head;
+        T* avail;
         T* tail;
 
+        std::allocator<T> alloc;
+
         void construct();
+        void append(T e);
         void destroy();
 
 };
 
+template <class T>
+size_type vector<T>::size() {
+    return avail - head;
+}
 
 template <class T>
 void vector<T>::push_back(T e) {
-    
+    append(e);
 }
 
+template <class T>
+void vector<T>::append(T e) {
+    //If capacity has been reached
+    if(avail == tail) {
+        //Double size
+        size = std::max(1, 2 * size());
+        //Allocate new space
+        T* n = alloc.allocate(size);
+        //Copy
+        std::uninitialized_copy(head, avail, n);
+        //Destroy
+        destroy();
+        //Append
+        alloc.construct(avail, e);
+        ++avail;
+        
+    } else {
+        alloc.construct(avail, e);
+        ++avail;
+    }
+
+}
 
 }
 
